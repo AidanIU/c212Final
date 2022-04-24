5688e29047a05dcc9814fc7d5d73f2a5607c58a4
PinkQuartz <aimoran@protonmail.com>
2022-04-24 19:58:45 -0400

    adding command structure

--- src/programs/Store.java
+++ src/programs/Store.java
@@ -4,6 +4,7 @@ import models.Item;
 import models.Staff;
 import utils.FileUtils;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -13,19 +14,20 @@ public abstract class Store implements IStore{
 
     public void takeAction() {
         //ADD '<itemName>' <itemCost> <itemQuantity>
+        //SAW
         //COST '<itemName>'
         //EXIT
         //FIND '<itemName>'
         //FIRE '<staffName>'
         //HIRE '<staffName>' <age> <role>
-        //PROMOTE '<staffName>' <role>
-        //SAW
-        //SCHEDULE
         //SELL '<itemName>' <quantity>
+        //PROMOTE '<staffName>' <role>
+        //SCHEDULE
         //QUANTITY '<itemName>'
 
         List<String> inputList = new ArrayList<String>();
         List<Staff> staffList = new ArrayList<Staff>();
+        List<Item> inventoryList = new ArrayList<Item>();
 
         try {
             inputList = FileUtils.readCommandsFromFile();
@@ -39,10 +41,30 @@ public abstract class Store implements IStore{
             e.printStackTrace();
         }
 
+        try {
+            inventoryList = FileUtils.readInventoryFromFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        for (String command: inputList){
+            if (command.substring(0,2) == "ADD"){}
+            else if (command.substring(0,2) == "SAW"){}
+            else if (command.substring(0,3) == "COST"){}
+            else if (command.substring(0,3) == "EXIT"){}
+            else if (command.substring(0,3) == "FIND"){}
+            else if (command.substring(0,3) == "FIRE"){}
+            else if (command.substring(0,3) == "HIRE"){}
+            else if (command.substring(0,3) == "SELL"){}
+            else if (command.substring(0,7) == "PROMOTE"){}
+            else if (command.substring(0,8) == "SCHEDULE"){}
+            else if (command.substring(0,8) == "QUANTITY"){}
+        }
 
     }
 
     public Store(){
-
+            takeAction();
         }
 }
