8cc3e1ad29c8b4713cb8ef8570b45853551abdce
pheile <pheile@iu.edu>
2022-04-26 14:20:35 -0400

    Fourth commit that had vwery little changes also I still ahve not tested my code

--- src/programs/StaffScheduler.java
+++ src/programs/StaffScheduler.java
@@ -12,23 +12,23 @@ public class StaffScheduler {
 //    private ArrayList<String> schedule = new ArrayList<>();
 //    private int totalTime = 0;
 
-    public StaffScheduler() throws FileNotFoundException {
-        updateNamesAvailability();
-        sortWorkPeople();
-    }
+//    public StaffScheduler() throws FileNotFoundException {
+//        updateNamesAvailability();
+//        sortWorkPeople();
+//    }
 
-    private void updateNamesAvailability() throws FileNotFoundException {
-        File inputFile= new File("C:\\Users\\peter\\OneDrive\\Documents\\C212\\c212Final-main\\src\\resources\\staff_availability_IN.txt");
-        Scanner in = new Scanner(inputFile);
-
-        while(in.hasNext()) {
-            String temp = in.nextLine();
-            String[] info = {temp.substring(0, temp.indexOf(" ", 2)),
-                    temp.substring(temp.indexOf(" ", 4) + 1)};
-            namesAvailability.add(info);
-        }
-        in.close();
-    }
+//    private void updateNamesAvailability() throws FileNotFoundException {
+//        File inputFile= new File("C:\\Users\\peter\\OneDrive\\Documents\\C212\\c212Final-main\\src\\resources\\staff_availability_IN.txt");
+//        Scanner in = new Scanner(inputFile);
+//
+//        while(in.hasNext()) {
+//            String temp = in.nextLine();
+//            String[] info = {temp.substring(0, temp.indexOf(" ", 2)),
+//                    temp.substring(temp.indexOf(" ", 4) + 1)};
+//            namesAvailability.add(info);
+//        }
+//        in.close();
+//    }
 
 //    private void updateStoreHours() throws FileNotFoundException {
 //        File inputFile= new File("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\shift_schedules_IN.txt");
@@ -45,24 +45,43 @@ public class StaffScheduler {
 //        in.close();
 //    }
 
-    private void sortWorkPeople(){
-        for (int i = 1; i < this.namesAvailability.size(); i++){
-            int index = i;
-            while((this.namesAvailability.get(i)[1].chars().filter(ch -> ch == '.').count()
-            <this.namesAvailability.get(i-1)[1].chars().filter(ch -> ch == '.').count())&& (index>0)){
-                String[] temp = this.namesAvailability.get(i);
-                this.namesAvailability.set(i,this.namesAvailability.get(i));
-                this.namesAvailability.set(i-1,temp);
-            }
-        }
-    }
+//    private void sortWorkPeople(){
+//        for (int i = 1; i < this.namesAvailability.size(); i++){
+//            int index = i;
+//            while((this.namesAvailability.get(i)[1].chars().filter(ch -> ch == '.').count()
+//            <this.namesAvailability.get(i-1)[1].chars().filter(ch -> ch == '.').count())&& (index>0)){
+//                String[] temp = this.namesAvailability.get(i);
+//                this.namesAvailability.set(i,this.namesAvailability.get(i));
+//                this.namesAvailability.set(i-1,temp);
+//            }
+//        }
+//    }
 
 
     public void scheduleStaff() throws FileNotFoundException {
         String[] weekdays = {"M","T","W","TR","F","SAT","SUN"};
 
-        PrintWriter out = new PrintWriter("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\store_schedule_OUT.txt");
+        File inputFile= new File("C:\\Users\\peter\\OneDrive\\Documents\\C212\\c212Final-main\\src\\resources\\staff_availability_IN.txt");
+        Scanner in = new Scanner(inputFile);
+        while(in.hasNext()) {
+            String temp = in.nextLine();
+            String[] info = {temp.substring(0, temp.indexOf(" ", 2)),
+                    temp.substring(temp.indexOf(" ", 4) + 1)};
+            namesAvailability.add(info);
+        }
+        in.close();
 
+        for (int i = 1; i < this.namesAvailability.size(); i++){
+            int index = i;
+            while((this.namesAvailability.get(i)[1].chars().filter(ch -> ch == '.').count()
+                    <this.namesAvailability.get(i-1)[1].chars().filter(ch -> ch == '.').count())&& (index>0)){
+                String[] temp = this.namesAvailability.get(i);
+                this.namesAvailability.set(i,this.namesAvailability.get(i));
+                this.namesAvailability.set(i-1,temp);
+            }
+        }
+
+        PrintWriter out = new PrintWriter("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\store_schedule_OUT.txt");
         for (int i = 0; i < 7; i++){
             int count = 0;
             out.print(weekdays[i]+ " ");
