aed7ef433524cdef78a422a76a9c74ae55b86430
pheile <pheile@iu.edu>
2022-04-26 14:16:52 -0400

    Third commit so suck my

--- src/programs/StaffScheduler.java
+++ src/programs/StaffScheduler.java
@@ -8,13 +8,12 @@ import java.util.Scanner;
 
 public class StaffScheduler {
     private ArrayList<String[]>  namesAvailability = new ArrayList<>();
-    private ArrayList<Integer> storeHours = new ArrayList<>();
-    private ArrayList<String> schedule = new ArrayList<>();
-    private int totalTime = 0;
+//    private ArrayList<Integer> storeHours = new ArrayList<>();
+//    private ArrayList<String> schedule = new ArrayList<>();
+//    private int totalTime = 0;
 
     public StaffScheduler() throws FileNotFoundException {
         updateNamesAvailability();
-        updateStoreHours();
         sortWorkPeople();
     }
 
@@ -28,21 +27,23 @@ public class StaffScheduler {
                     temp.substring(temp.indexOf(" ", 4) + 1)};
             namesAvailability.add(info);
         }
+        in.close();
     }
 
-    private void updateStoreHours() throws FileNotFoundException {
-        File inputFile= new File("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\shift_schedules_IN.txt");
-        Scanner in = new Scanner(inputFile);
-
-        while(in.hasNext()) {
-            String temp = in.nextLine();
-            int open = Integer.parseInt(temp.substring(temp.indexOf(" ", 1)+1,temp.indexOf(" ", 2)));
-            int close = Integer.parseInt(temp.substring(temp.indexOf(" ", 2)+1));
-            int time = (close - open)*60;
-            this.totalTime += time;
-            storeHours.add(time);
-        }
-    }
+//    private void updateStoreHours() throws FileNotFoundException {
+//        File inputFile= new File("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\shift_schedules_IN.txt");
+//        Scanner in = new Scanner(inputFile);
+//
+//        while(in.hasNext()) {
+//            String temp = in.nextLine();
+//            int open = Integer.parseInt(temp.substring(temp.indexOf(" ", 1)+1,temp.indexOf(" ", 2)));
+//            int close = Integer.parseInt(temp.substring(temp.indexOf(" ", 2)+1));
+//            int time = (close - open)*60;
+//            this.totalTime += time;
+//            storeHours.add(time);
+//        }
+//        in.close();
+//    }
 
     private void sortWorkPeople(){
         for (int i = 1; i < this.namesAvailability.size(); i++){
@@ -57,30 +58,23 @@ public class StaffScheduler {
     }
 
 
-
-//    File inputFile = new File(fileName);
-//    Scanner in = new Scanner(inputFile);
-//
-//while (in.hasNext()) {
-//        String temp = in.nextLine();
-//
-//    PrintWriter out = new PrintWriter(fileName);
-//        for (String i: patients){
-//        out.println(i);
-//    }
-//        out.close();
-
-    public void scheduleStaff(){
+    public void scheduleStaff() throws FileNotFoundException {
         String[] weekdays = {"M","T","W","TR","F","SAT","SUN"};
-        int count = 0;
-
-        for (int i = 0; i < 7; i++){
-            for (int x = 0; x < namesAvailability.size(); x++){
-                if ()
-            }
-        }
 
         PrintWriter out = new PrintWriter("C:\\Users\\peter\\IdeaProjects\\c212Final\\src\\resources\\store_schedule_OUT.txt");
+
+        for (int i = 0; i < 7; i++){
+            int count = 0;
+            out.print(weekdays[i]+ " ");
+            for (int x = 0; x < this.namesAvailability.size(); x++){
+                if ((namesAvailability.get(x)[0].contains("."+weekdays[i]) || namesAvailability.get(x)[0].contains(weekdays[i]+".")) && count!=3){
+                    out.print("("+namesAvailability.get(x)+") ");
+                    namesAvailability.add(namesAvailability.get(x));
+                    namesAvailability.remove(x);
+                }
+            }
+        }
+        out.close();
     }
 }
 
