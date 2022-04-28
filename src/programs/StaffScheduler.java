0eed4cc7b23b39931b3ae4ed965abf4750be4cf0
pheile <pheile@iu.edu>
2022-04-27 23:02:56 -0400

    LAST COMMIT BITCHES

--- src/programs/StaffScheduler.java
+++ src/programs/StaffScheduler.java
@@ -16,6 +16,18 @@ public class StaffScheduler {
             peopleInfo = readStaffFromFile();
             PrintWriter out = new PrintWriter("src/resources/store_schedule_OUT.txt");
 
+            //puts people in order starting with those working the fewest days to make sure they get at
+            //least one work day if they can work very few days
+            char search = '.';
+            for (int i =1; i < peopleInfo.size(); i++){
+                int index = 0;
+                while((peopleInfo.get(i).getAvailability().chars().filter(ch -> ch == search).count()
+                <peopleInfo.get(i-1).getAvailability().chars().filter(ch -> ch == search).count())&&index!=0){
+                    peopleInfo.add(peopleInfo.get(i));
+                    peopleInfo.remove(i);
+                }
+            }
+
             for (int i = 0; i < 7; i++) {
                 int count = 0;
                 out.print(weekdays[i] + " ");
